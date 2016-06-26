f3154783b7134de90d45afa4404dbbfa86755a50
Charlie Yang <charlieyang@Yangs-MacBook-Pro.local>
2016-06-25 19:31:21 -0700

    switch between routes complete

--- client/javascripts/components.js
+++ client/javascripts/components.js
@@ -3,28 +3,40 @@
   angular
     .module('charlieRun')
     .component('cyAbout',{
-      bindings:{},
+      bindings:{
+        exit: '<',
+        expand: '<',
+      },
       controller:'aboutController',
       controllerAs:'vm',
       templateUrl:'../views/about.html'
     })
 
     .component('cyPortfolio',{
-      bindings:{},
+      bindings:{
+        exit: '<',
+        expand: '<',
+      },
       controller:'portfolioController',
       controllerAs:'vm',
       templateUrl:'../views/portfolio.html'
     })
 
     .component('cyResume',{
-      bindings:{},
+      bindings:{
+        exit: '<',
+        expand: '<',
+      },
       controller:'resumeController',
       controllerAs:'vm',
       templateUrl:'../views/resume.html'
     })
 
     .component('cyContact',{
-      bindings:{},
+      bindings:{
+        exit: '<',
+        expand: '<',
+      },
       controller:'contactController',
       controllerAs:'vm',
       templateUrl:'../views/contact.html'
--- client/javascripts/controller.js
+++ client/javascripts/controller.js
@@ -8,16 +8,53 @@
 		.controller("resumeController", resumeController)
 		.controller("contactController", contactController)
 
-	function mainController(){
+	function mainController($interval){
 		let vm = this;
 		vm.msg = "main";
+		vm.skills= ['EXPRESS','NODE','ANGULAR','REACT'];
+
+		let idx=0,
+				n=0,
+				up=true;
+
+		$interval(function(){
+			let word = vm.skills[idx];
+			let ln = word.length;
+
+			if(up){
+				vm.type = word.slice(0,n);
+				n++
+			};
+			if(n==ln+1){up=false};
+			if(!up){
+				vm.type = word.slice(0,n);
+				n--
+			};
+			if(n==0){
+				up=true;
+				idx++
+			};
+			if(idx==vm.skills.length){idx=0}
+		}, 200)
 	}
 
-	function subController($location){
+	function subController($location, $timeout){
 		let vm = this;
 		vm.msg = "sub";
 		vm.current = $location.path()
+		vm.expand = false;
 
+		vm.exit = function(){
+			vm.expand = false;
+
+			$timeout(function(){
+				$location.path('/');
+			}, 1000);
+		}
+
+		$timeout(function(){
+			vm.expand = true;
+		}, 10);
 	}
 
 	function aboutController($location){
